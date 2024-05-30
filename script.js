f1b7f5e2e6477110071c832299099490ffe872ab
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-05-30 19:34:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let ism = prompt("String typedagi malumotni kiriting");
+let familiya = prompt("Number typedagi malumotni kiriting");
+let yosh = prompt("Booleon kiriting");
+
+let confirm = ("")
+alert("Malumotingiz togriligiga ishonchingiz komilmisiz?")
+
